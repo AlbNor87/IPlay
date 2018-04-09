d50e91830eac0b031e9765bcdadcc586a31bba04
AlbNor87 <albert.norberg@gmail.com>
2018-04-09 18:39:48 +0200

    Cleaned up some code

--- public/themes/wordplate/index.php
+++ public/themes/wordplate/index.php
@@ -1,9 +1,5 @@
 <?php get_header(); ?>
 
-
-
-
-
 <main role="main">
     <?php if (have_posts()): while (have_posts()): the_post(); ?>
         <article>
@@ -19,19 +15,5 @@
         </article>
     <?php endif; ?>
 </main>
-
-
-    <!-- <div id="vidtop-content">
-    <div class="vid-info">
-        <h1>YouTube Fullscreen Background Demo</h1>
-        <p>The International Space Station orbits the Earth every 92 minutes, with its crew seeing a sunrise 15 times a day. It exists as a scientific, educational, and engineering platform in low orbit, 330 to 435 kilometres above the Earth.
-        <p>Original timelapse by Riccardo Rossi (ISAA), used under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License. Raw photos courtesy of http://eol.jsc.nasa.gov/
-        <a href="/500/Use-YouTube-Videos-as-Fullscreen-Web-Page-Backgrounds">Full article</a>
-    </div>
-    </div> -->
-
-
-
-
 
 <?php get_footer();
