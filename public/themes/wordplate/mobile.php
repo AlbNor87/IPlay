961a8205ca80aa420951ac11c3d15584d841aa89
AlbNor87 <albert.norberg@gmail.com>
2018-04-12 23:08:06 +0200

    updated mobile

--- public/themes/wordplate/mobile.php
+++ public/themes/wordplate/mobile.php
@@ -1,7 +1,17 @@
     <!-- MOBILE START-->
     <div class="mobile">
 
-    <div class="mob-nav"></div>
+        <div class="mob-nav">
+
+            <img src="/themes/wordplate/images/iplaylogo.svg" alt="logo" class="mob-logo">
+
+        </div><!-- mob-nav -->
+
+        <div class="mob-container">
+        
+            <img src="/themes/wordplate/images/mockup.png" alt="logo" class="mob-mockup">
+
+        </div><!-- mob-container -->
 
     </div>
     <!-- MOBILE END -->
