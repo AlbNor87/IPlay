9e23225127d01e838b97b11ecd4b67a52d2a4027
AlbNor87 <albert.norberg@gmail.com>
2018-04-13 13:24:45 +0200

    Whole lotta changes

--- public/themes/wordplate/index.php
+++ public/themes/wordplate/index.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
-<?php require template_path('/shared.php');?>
-<?php require template_path('/desktop.php');?>
-<?php require template_path('/mobile.php');?>
+<?php require template_path('components/videobg.php');?>
+<?php require template_path('components/desktop/desktop.php');?>
+<?php require template_path('components/mobile/mobile.php');?>
 
 <?php get_footer();
