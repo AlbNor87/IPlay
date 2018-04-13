164781e7a482844a15d58ca2596d358e39061d7e
AlbNor87 <albert.norberg@gmail.com>
2018-04-13 11:07:39 +0200

    Updated Mobile

--- public/themes/wordplate/mobile.php
+++ public/themes/wordplate/mobile.php
@@ -5,6 +5,28 @@
 
             <img src="/themes/wordplate/images/iplaylogo.svg" alt="logo" class="mob-logo">
 
+            <div class="mobile-menu">
+
+                <div id="menuToggle">
+
+                    <input type="checkbox"/>
+
+                    <span></span>
+                    <span></span>
+                    <span></span>
+
+                    <ul id="menu">
+                        <a href="/index.php"><li>Home</li></a>
+                        <a href="/main.php"><li>Features</li></a>
+                        <a href="/about.php"><li>Press</li></a>
+                        <a href="/about.php"><li>Contact</li></a>
+                        <a href="/about.php"><li>About Us</li></a>
+                    </ul>
+
+                </div><!-- menuToggle -->
+
+            </div><!-- mobile-menu -->
+
         </div><!-- mob-nav -->
 
         <div class="mob-container">
