84c595dad91f6f3564474b5fbd9ffb200a031f46
AlbNor87 <albert.norberg@gmail.com>
2018-04-14 18:30:22 +0200

    Fixed temp font

--- public/themes/wordplate/header.php
+++ public/themes/wordplate/header.php
@@ -4,6 +4,7 @@
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="theme-color" content="#6d9aea">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
