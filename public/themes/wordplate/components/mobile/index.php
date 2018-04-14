97778685ce3201f9e5395e3e2f931b1ec77a6e2d
AlbNor87 <albert.norberg@gmail.com>
2018-04-15 01:16:25 +0200

    updated mobile

--- public/themes/wordplate/components/mobile/index.php
+++ public/themes/wordplate/components/mobile/index.php
@@ -5,11 +5,24 @@
     
         <img src="/themes/wordplate/images/mockup.png" alt="logo" class="mob-mockup">
 
-        <div class="mob-unitingTheWorld">
+        <div class="mob-slogan">
+            
             <h1>UNITING <br>
                 THE WORLD
             <br>OF SPORT</h1>
+            
+            <div class="mob-tagLine">
+                <p>Iplay is the tool for athletes to <br>
+                grow their personal brand and <br>
+                for fans following their journey.</p>
+            </div>
+
+            <img src="themes/wordplate/images/appstoreblack.svg" alt="appstore" class="mob-appstore">
+            <img src="themes/wordplate/images/googleplay.png" alt="googleplay" class="mob-googleplay">
+
         </div>
+
+        
 
     </div><!-- mob-container -->
 
