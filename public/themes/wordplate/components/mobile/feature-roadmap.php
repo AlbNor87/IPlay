58eff7cbd05ad1ff6a7235ece5490abdf7c067b6
AlbNor87 <albert.norberg@gmail.com>
2018-04-15 18:50:10 +0200

    update

--- public/themes/wordplate/components/mobile/feature-roadmap.php
+++ public/themes/wordplate/components/mobile/feature-roadmap.php
@@ -3,7 +3,30 @@
 
     <div class="mob-container">
     
-        <H1>Welcome to the MOBILE Feature Roadmap page!</H1>
+    <img src="themes/wordplate/images/roadmap.svg" alt="roadMap" class="mob-roadMap">
+
+    <img src="themes/wordplate/images/googleplay.png" alt="googleplay" class="mob-googleplay">
+
+
+    <div class="mob-athlete-box">
+
+<div class="mob-row center">
+    <h1>I’m an athlete</h1>
+</div><!-- mob-row -->
+
+<div class="mob-row center">
+    <p>Start building your personal brand <br>
+     in the digital world.</p>
+</div><!-- mob-row -->
+
+<div class="mob-row center">
+    <div class="buttons">
+        <div class="mob-btn"><h3>Learn more</h3></div>
+        <div class="mob-btn"><h3>Get started</h3></div>
+    </div><!-- buttons -->
+</div><!-- mob-row -->
+
+</div><!-- mob-athlete-box -->
 
     </div><!-- mob-container -->
 
