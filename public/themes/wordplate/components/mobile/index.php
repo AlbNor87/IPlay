31c9f3f670213fe264b4e694c13b95f727f5cb6b
AlbNor87 <albert.norberg@gmail.com>
2018-04-15 12:47:56 +0200

    Updated mobile version

--- public/themes/wordplate/components/mobile/index.php
+++ public/themes/wordplate/components/mobile/index.php
@@ -8,8 +8,8 @@
         <div class="mob-slogan">
             
             <h1>UNITING <br>
-                THE WORLD
-            <br>OF SPORT</h1>
+                THE WORLD<br>
+                OF SPORT</h1>
             
             <div class="mob-tagLine">
                 <p>Iplay is the tool for athletes to <br>
@@ -20,9 +20,15 @@
             <img src="themes/wordplate/images/appstoreblack.svg" alt="appstore" class="mob-appstore">
             <img src="themes/wordplate/images/googleplay.png" alt="googleplay" class="mob-googleplay">
 
-        </div>
+        </div><!-- mob-slogan -->
 
-        
+        <img src="themes/wordplate/images/downarrow.png" alt="scroll-down arrow" class="mob-arrow-down">
+
+        <div class="mob-landingpage-placeholder"></div>
+
+        <div class="mob-athlete-box"></div>
+        <div class="mob-fans-box"></div>
+        <div class="mob-others-box"></div>
 
     </div><!-- mob-container -->
 
