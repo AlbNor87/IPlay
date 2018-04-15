a52ede72a18d1a630b438511e9a5f2eac8c8862f
AlbNor87 <albert.norberg@gmail.com>
2018-04-15 20:47:06 +0200

    Updated stuff

--- public/themes/wordplate/components/desktop/index.php
+++ public/themes/wordplate/components/desktop/index.php
@@ -1,33 +1,38 @@
-    <!-- DESKTOP START-->
-    <div class="desktop">
+<!-- DESKTOP START-->
+<div class="desktop">
 
-        <div class="title">
-            <p>UNITING</p>
-            <p>THE WORLD</p>
-            <p>OF SPORT</p>
-        </div>
+    <div class="desktop-placeholder"></div>
 
-        <div class="titletext">
-            <p>Iplay is the tool for athletes to</p>
-            <p>expand their personal brand and for</p>
-            <p>fans to follow their journey.</p>
-        </div>
+    <div class="title">
+        <p>UNITING</p>
+        <p>THE WORLD</p>
+        <p>OF SPORT</p>
+    </div>
 
-        <div class="app">
+    <div class="titletext">
+        <p>Iplay is the tool for athletes to</p>
+        <p>expand their personal brand and for</p>
+        <p>fans to follow their journey.</p>
+    </div>
+
+    <div class="app">
         <img src="themes/wordplate/images/appstoreblack.svg" alt="appstore" class="app1">
         <img src="themes/wordplate/images/googleplay.png" alt="appstore" class="app2">
-        </div>
+    </div>
 
-        <img src="themes/wordplate/images/mockup.png" alt="" class="mockup">
+    <img src="themes/wordplate/images/mockup.png" alt="" class="mockup">
 
-        <div class="boxes">
-            <div class="other"></div>
-            <div class="fan"></div>
-            <div class="athlete"></div>
-        </div>
+    <div class="desktop-whiteBgSquare"></div>
 
-        <div class="superusers">Superusers</div>
+    <div class="boxes">
+        <div class="other"></div>
+        <div class="fan"></div>
+        <div class="athlete"></div>
+    </div>
 
+    <div class="superusers">Superusers</div>
+
+    <div class="desktop-row right">
         <div class="testimonial1">
             <img src="themes/wordplate/images/nathaliehagman.png" alt="" class="testimonialimage1">
             <div class="athletetext1">
@@ -37,36 +42,37 @@
                 <p class="quote1">"Iplay är den plattform som vi tidigare har saknat.<br> Skapad för oss atleter med våra unika behov."</p>
             </div>
         </div>
+    </div><!-- desktop-row -->
 
-        <div class="testimonial2">
-            <img src="themes/wordplate/images/victorthomas.png" alt="" class="testimonialimage2">
-            <div class="athletetext2">
-                <h2 class="athletename2">Victor Tomás</h2>
-                <p class="team2">FC BARCELONA</p>
-                <br>
-                <p class="quote2">"Iplay is a unique tool that will really<br> make a difference for us athletes."</p>
-            </div>
+    <div class="testimonial2">
+        <img src="themes/wordplate/images/victorthomas.png" alt="" class="testimonialimage2">
+        <div class="athletetext2">
+            <h2 class="athletename2">Victor Tomás</h2>
+            <p class="team2">FC BARCELONA</p>
+            <br>
+            <p class="quote2">"Iplay is a unique tool that will really<br> make a difference for us athletes."</p>
         </div>
-
-        <div class="testimonial3">
-            <img src="themes/wordplate/images/nicklasekberg.png" alt="" class="testimonialimage3">
-            <div class="athletetext3">
-                <h2 class="athletename3">Niclas Ekberg</h2>
-                <p class="team3">THW Kiel</p>
-                <br>
-                <p class="quote3">"På Iplay har jag full kontroll över min kommunikation och kan bygga mitt varumärke ännu starkare"</p>
-            </div>
-        </div>
-
-<div class="desktopfooter">
-    <div class="footertext">
-        <img src="themes/wordplate/images/iplaylogo_white.svg" alt="wut" class="footerlogo">
-        <p class="footerlinks">About Careers Press Contact Blog</p>
-        <p class="footerphoneaddress">+46 725 455 900<br>Björckgatan 45A Göteborg</p>
-        <p class="footercopyright">2016 All rights reserved. IPY Holding AB</p>
     </div>
 
-</div>
-
+    <div class="testimonial3">
+        <img src="themes/wordplate/images/nicklasekberg.png" alt="" class="testimonialimage3">
+        <div class="athletetext3">
+            <h2 class="athletename3">Niclas Ekberg</h2>
+            <p class="team3">THW Kiel</p>
+            <br>
+            <p class="quote3">"På Iplay har jag full kontroll över min kommunikation och kan bygga mitt varumärke ännu starkare"</p>
+        </div>
     </div>
-    <!-- DESKTOP END -->
+
+    <div class="desktopfooter">
+        <div class="footertext">
+            <img src="themes/wordplate/images/iplaylogo_white.svg" alt="wut" class="footerlogo">
+            <p class="footerlinks">About Careers Press Contact Blog</p>
+            <p class="footerphoneaddress">+46 725 455 900<br>Björckgatan 45A Göteborg</p>
+            <p class="footercopyright">2016 All rights reserved. IPY Holding AB</p>
+        </div>
+
+    </div><!-- desktop-footer -->
+
+</div><!-- desktop -->
+<!-- DESKTOP END -->
