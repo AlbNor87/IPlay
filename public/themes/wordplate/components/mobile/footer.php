9b365543c629df76d1d9506db8dd48a223c0eff6
AlbNor87 <albert.norberg@gmail.com>
2018-04-16 22:20:49 +0200

    Updated footer

--- public/themes/wordplate/components/mobile/footer.php
+++ public/themes/wordplate/components/mobile/footer.php
@@ -14,11 +14,23 @@
 
     <div class="mob-row center">
         <div class="mob-social-icons">
-        <img src="/themes/wordplate/images/facebook-logo-button.svg" alt="facebook" class="mob-icon">
-        <img src="/themes/wordplate/images/instagram-logo-button.svg" alt="instagram" class="mob-icon">
-        <img src="/themes/wordplate/images/linkedin-logo-button.svg" alt="linkedin" class="mob-icon">
-        <img src="/themes/wordplate/images/twitter-logo-button.svg" alt="twitter" class="mob-icon">
+            <a href="https://www.facebook.com/iplay.sportofficial"><img src="/themes/wordplate/images/facebook-logo-button.svg" alt="facebook" class="mob-icon"></a>
+            <a href="https://www.instagram.com/iplay.global/"><img src="/themes/wordplate/images/instagram-logo-button.svg" alt="instagram" class="mob-icon"></a>
+            <a href="https://www.linkedin.com/company/ipy-holding-ab/"><img src="/themes/wordplate/images/linkedin-logo-button.svg" alt="linkedin" class="mob-icon"></a>
+            <a href="https://twitter.com/IPLAY_GLOBAL"><img src="/themes/wordplate/images/twitter-logo-button.svg" alt="twitter" class="mob-icon"></a>
         </div><!-- social-icons -->
     </div><!-- mob-row -->
+
+    <div class="mob-row center">
+        <div class="mob-email">
+        <input type="text" placeholder="YOUR EMAIL...">
+        <button class="mob-mail-btn"></button>
+        </div><!-- mob-email --> 
+    </div><!-- mob-row -->
+
+    
+        <p class="mob-copyright">Västra Hamngatan 7C Göteborg <br>
+        © 2016 All rights reserved. IPY Holding AB</p>
+    
 
 </div><!-- mob-footer -->
