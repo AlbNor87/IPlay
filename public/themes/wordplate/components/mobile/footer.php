f9f23f081f7ef1e685a8f4c7960ac735f1922b47
AlbNor87 <albert.norberg@gmail.com>
2018-04-16 21:12:50 +0200

    added social icons

--- public/themes/wordplate/components/mobile/footer.php
+++ public/themes/wordplate/components/mobile/footer.php
@@ -13,7 +13,12 @@
     </div><!-- mob-row -->
 
     <div class="mob-row center">
-        <div class="mob-social-icons"> </div>
+        <div class="mob-social-icons">
+        <img src="/themes/wordplate/images/facebook-logo-button.svg" alt="facebook" class="mob-icon">
+        <img src="/themes/wordplate/images/instagram-logo-button.svg" alt="instagram" class="mob-icon">
+        <img src="/themes/wordplate/images/linkedin-logo-button.svg" alt="linkedin" class="mob-icon">
+        <img src="/themes/wordplate/images/twitter-logo-button.svg" alt="twitter" class="mob-icon">
+        </div><!-- social-icons -->
     </div><!-- mob-row -->
 
 </div><!-- mob-footer -->
