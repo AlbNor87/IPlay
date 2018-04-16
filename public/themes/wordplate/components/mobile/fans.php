1f7f7430cc0724f83e12dcff192793bc8c5dbd6c
AlbNor87 <albert.norberg@gmail.com>
2018-04-16 15:09:27 +0200

    Added athletes and fans pages

--- public/themes/wordplate/components/mobile/fans.php
+++ public/themes/wordplate/components/mobile/fans.php
@@ -2,10 +2,50 @@
 <div class="mobile">
 
     <div class="mob-container">
-    
-        <H1>Welcome to the MOBILE fans page!</H1>
+
+        <div class="mob-row center">
+        <h1 class="mob-title">Features for fans</h1>
+        </div><!-- mob-row -->
+
+        <div class="mob-row center">
+            <div class="mob-content">
+                <img src="/themes/wordplate/images/sports_pink.png" alt="sports" class="mob-img-sport">       
+                <h3>MULTITUDE OF SPORTS</h3>
+                <p>We offer support and functionality for <br>
+                several sports and we are still adding more!</p>
+            </div><!-- mob-content -->
+        </div><!-- mob-row -->
+
+        <div class="mob-row center">
+            <div class="mob-content">
+                <img src="/themes/wordplate/images/crown_pink.png" alt="crown" class="mob-img-crown">       
+                <h3>FOLLOW THE BEST</h3>
+                <p>Follow the best athletes in the world. Keep <br>
+                up to date with their careers and life.</p>
+            </div><!-- mob-content -->
+        </div><!-- mob-row -->
+
+        <div class="mob-row center">
+            <div class="mob-content">
+                <img src="/themes/wordplate/images/game_pink.png" alt="game" class="mob-img-game">       
+                <h3>ALWAYS GAME READY</h3>
+                <p>See live results from matches and get<br>
+                notified when something happens.</p>
+            </div><!-- mob-content -->
+        </div><!-- mob-row -->
+
+        <div class="mob-row center">
+            <div class="mob-content">
+                <img src="/themes/wordplate/images/chat_pink.png" alt="chat" class="mob-img-chat">       
+                <h3>CHAT AND SHARE</h3>
+                <p>Chat and share images and <br>
+                videos with your friends.</p>
+            </div><!-- mob-content -->
+        </div><!-- mob-row -->
 
     </div><!-- mob-container -->
 
 </div>
 <!-- MOBILE END -->
+
+
