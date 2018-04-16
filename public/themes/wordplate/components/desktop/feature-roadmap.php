19db25197023558ae2257bc2d42132decde86bb5
AlbNor87 <albert.norberg@gmail.com>
2018-04-17 00:59:05 +0200

    Updated roadmap

--- public/themes/wordplate/components/desktop/feature-roadmap.php
+++ public/themes/wordplate/components/desktop/feature-roadmap.php
@@ -3,10 +3,20 @@
 
     <div class="roadmap-container">
 
-    <h1>ROADMAP</h1>
-    <p>Donec facilisis tortor ut augue lacinia, at<br> viverra est semper. Sed sapien metus,<br> scelerisque nec.</p>
+    <div class="row"></div>
+    <div class="roadmap-title">
+        <h1>ROADMAP</h1>
+        
+        <p>Donec facilisis tortor ut augue lacinia, at<br>
+        viverra est semper. Sed sapien metus,<br>
+        scelerisque nec.</p>
+    </div><!-- roadmap-title -->
+
+    <div class="roadmap-scroll">
         <img src="/themes/wordplate/images/roadmapDesktop.svg" alt="roadMap" class="desktoproadmap">
-    </div>
+    </div><!-- roadmap-scroll -->
+    </div><!-- roadmap-container -->
+    
 
 </div><!-- desktop -->
 <!-- DESKTOP END -->
