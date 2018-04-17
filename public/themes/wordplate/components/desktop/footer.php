5e83472e6e37537491ccb8e8221b00e6c7956575
AlbNor87 <albert.norberg@gmail.com>
2018-04-17 03:18:33 +0200

    More updates

--- public/themes/wordplate/components/desktop/footer.php
+++ public/themes/wordplate/components/desktop/footer.php
@@ -17,7 +17,26 @@
 
 
     <div class="footer-right">
-        
+
+        <h3>Prenumera på nyhetsbrevet för<br>
+        nyheter!</h3>
+
+        <div class="mob-row right">
+            <div class="mob-email">
+            <input type="text" placeholder="YOUR EMAIL...">
+            <button class="mob-mail-btn"></button>
+            </div><!-- mob-email --> 
+        </div><!-- mob-row -->
+
+        <div class="mob-row right">
+            <div class="desk-social-icons">
+                <a href="https://www.facebook.com/iplay.sportofficial"><img src="/themes/wordplate/images/facebook-logo-button.svg" alt="facebook" class="desk-icon"></a>
+                <a href="https://www.instagram.com/iplay.global/"><img src="/themes/wordplate/images/instagram-logo-button.svg" alt="instagram" class="desk-icon"></a>
+                <a href="https://www.linkedin.com/company/ipy-holding-ab/"><img src="/themes/wordplate/images/linkedin-logo-button.svg" alt="linkedin" class="desk-icon"></a>
+                <a href="https://twitter.com/IPLAY_GLOBAL"><img src="/themes/wordplate/images/twitter-logo-button.svg" alt="twitter" class="desk-icon"></a>
+            </div><!-- social-icons -->
+        </div><!-- mob-row -->
+
     </div><!-- footer-right -->
 
 
